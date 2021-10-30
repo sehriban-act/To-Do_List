bd294e4b60591a6372b12a26805c24eaaa1dd9bf
sehriban <sehribanmatur9@gmail.com>
2021-10-29 21:00:45 -0400

    edited from extras

--- btnclickcount/click.js
+++
@@ -1,8 +0,0 @@
-var count = 0;
-		var btn = document.getElementById("btn");
-		var disp = document.getElementById("display");
-
-		btn.onclick = function () {
-			count++;
-			disp.innerHTML = count;
-		}
